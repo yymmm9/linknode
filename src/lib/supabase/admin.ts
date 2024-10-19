dc514cd2d8b283b8a6edb892e6823b73db701677
yymmm9 <yymmm9@gmail.com>
2024-10-20 01:33:56 +0200

    env

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export default function supabaseAdmin() {
 	return createClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.SUPABASE_URL!,
 		process.env.SUPABAE_ADMIN!,
 		{
 			auth: {
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,8 +9,8 @@ import { createClient } from '@supabase/supabase-js';
 import { Dub } from 'dub';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 
 );
 
